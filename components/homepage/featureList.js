dbcdd63ffde7e6fbf5f380773aebc636b6ef44b5
reactexcel <rohit.eexcel2011@gmail.com>
2022-06-22 16:54:58 +0530

    landing page design

--- components/homepage/featureList.js
+++ components/homepage/featureList.js
@@ -8,12 +8,177 @@ const FeatureList = () => {
         <div style={{ marginBottom: '-18px', marginLeft: '-86px' }}>
           <Image src='/assets/feature.png' alt='' width='20px' height='20px' />
         </div>
-        <div className='text-4xl' style={{ letterSpacing: '-0.6px' }}>
+        <div className='text-5xl' style={{ letterSpacing: '-0.6px' }}>
           Now
           <br />
           <div className='font-extrabold py-3'>
             here’s the feature list you <br />
             <div className='py-3'> were looking for</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='w-10/12 mx-auto flex justify-around items-center py-10'>
+          <div>
+            <div className='text-4xl font-extrabold'>
+              Leadzilla prospecting <br /> platform
+            </div>
+            <div className='text-base font-semibold py-5'>
+              Convert more prospects into customers with targeted search
+              <br /> on over{' '}
+              <span className='text-blue-600'>20 search filters</span>
+            </div>
+
+            <div className='mt-4'>
+              <button className='bg-black p-3 px-8 text-base font-bold relative'>
+                Learn more
+                <button className='bg-yellow-500 p-3 px-8 text-base font-bold absolute bottom-2 left-2 whitespace-nowrap'>
+                  Learn more
+                </button>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <Image
+              src='/assets/platform.png'
+              alt=''
+              width='516px'
+              height='417px'
+            />
+          </div>
+        </div>
+        <div className='w-10/12 mx-auto flex justify-around items-center py-20'>
+          <div>
+            <Image
+              src='/assets/extansion.png'
+              alt=''
+              width='516px'
+              height='417px'
+            />
+          </div>
+          <div>
+            <div className='text-4xl font-extrabold'>Leadzilla extension</div>
+            <div className='text-base font-semibold py-5'>
+              <span className='text-blue-600'> Find, target and connect</span>{' '}
+              with ideal prospects on LinkedIn
+            </div>
+
+            <div className='mt-4'>
+              <button className='bg-black p-3 px-8 text-base font-bold relative'>
+                Learn more
+                <button className='bg-yellow-500 p-3 px-8 text-base font-bold absolute bottom-2 left-2 whitespace-nowrap'>
+                  Learn more
+                </button>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-10/12 mx-auto flex justify-around items-center py-10'>
+          <div>
+            <div className='text-4xl font-extrabold'>Contact search</div>
+            <div className='text-base font-semibold py-5'>
+              <div className='flex'>
+                <div className='mr-3'>
+                  <Image
+                    src='/assets/rectangle.png'
+                    alt=''
+                    width='10px'
+                    height='10px'
+                  />
+                </div>
+                <div>
+                  <span className='text-blue-600'>200 million+ contacts, 95%+ email accuracy, 100m+</span>
+                  <br /> <span className='text-blue-600'>mobile</span> numbers at your disposal.
+                </div>
+              </div>
+              <div className='flex'>
+                <div className='mr-3'>
+                  <Image
+                    src='/assets/rectangle.png'
+                    alt=''
+                    width='10px'
+                    height='10px'
+                  />
+                </div>
+                <div>
+                Built-in real-time email verification
+                </div>
+              </div>
+              <div className='flex'>
+                <div className='mr-3'>
+                  <Image
+                    src='/assets/rectangle.png'
+                    alt=''
+                    width='10px'
+                    height='10px'
+                  />
+                </div>
+                <div>
+                Highly advanced filters: <span className='text-blue-600'>Technographics,</span>
+                  <br /> <span className='text-blue-600'>firmographics and our patent-pending Lookalike AI</span>
+                </div>
+              </div>
+         
+         
+
+              {/* Convert more prospects into customers with targeted search
+              <br /> on over{' '}
+              <span className='text-blue-600'>20 search filters</span> */}
+            </div>
+
+            <div className='mt-4'>
+              <button className='bg-black p-3 px-8 text-base font-bold relative'>
+                Learn more
+                <button className='bg-yellow-500 p-3 px-8 text-base font-bold absolute bottom-2 left-2 whitespace-nowrap'>
+                  Learn more
+                </button>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <Image
+              src='/assets/contactsearch.png'
+              alt=''
+              width='578px'
+              height='425px'
+            />
+          </div>
+        </div>
+        <div className='w-10/12 mx-auto flex justify-around items-center py-20'>
+          <div>
+            <Image
+              src='/assets/extansion.png'
+              alt=''
+              width='516px'
+              height='417px'
+            />
+          </div>
+          <div>
+            <div className='text-4xl font-extrabold'>
+              AI-powered personalization
+            </div>
+            <div className='text-base font-semibold py-5'>
+              AI that writes highly personalized icebreaker lines
+              <br />
+              Patent-pending personality analyzer AI for highly relevant emails
+              <br />
+              Personalization lines based on{' '}
+              <span className='text-blue-600'>20+ data points </span> from the
+              web
+              {/* <span className='text-blue-600'> Find, target and connect</span> with ideal prospects on LinkedIn  */}
+            </div>
+
+            <div className='mt-4'>
+              <button className='bg-black p-3 px-8 text-base font-bold relative'>
+                Learn more
+                <button className='bg-yellow-500 p-3 px-8 text-base font-bold absolute bottom-2 left-2 whitespace-nowrap'>
+                  Learn more
+                </button>
+              </button>
+            </div>
           </div>
         </div>
       </div>
