696220107131652b686a825b9454e9e39bfd31d3
reactexcel <ashutoh.excel2011@gmail.com>
2022-07-04 16:38:10 +0530

    make a space change

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
      <Head>
-      <link rel="shortcut icon" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/assets/favicon.svg" />
     </Head>
       <Meta />
       <Component {...pageProps} />
