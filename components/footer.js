0cb201efffe63956db771a13b89d815c36e52e0a
Singh-Vikas-M <vikas3091999@gmail.com>
2022-07-04 15:37:12 +0530

    minor fixes

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
 import Link from "next/link";
 
 function Footer() {
   return (
     <>
       <div className="bg-black w-full flex justify-center">
-        <div className='w-10/12 py-20'>
-          <div className='flex flex-row justify-between'>
-            <div className='flex justify-center items-center'>
-              <div className='flex flex-row items-center'>
-                <Image src="/assets/footerlogoleadzilla.svg" alt="leadzilla logo" width="162px" height="42px" />
+        <div className="w-10/12 py-20">
+          <div className="flex flex-row justify-between">
+            <div className="flex justify-center items-center">
+              <div className="flex flex-row items-center">
+                <Image
+                  src="/assets/footerlogoleadzilla.svg"
+                  alt="leadzilla logo"
+                  width="162px"
+                  height="42px"
+                />
                 {/* <p className='text-white text-xl font-bold ml-2'>Leadzilla</p> */}
               </div>
               {/* <div className='flex flex-row items-center ml-4'>
@@ -19,60 +24,90 @@ function Footer() {
               </div> */}
             </div>
 
-            <div className='flex flex-row items-center'>
-            <Link href="https://app.leadzilla.ai/login">
-              <button className='text-white text-sm font-bold border-2 py-4 px-8 rounded-full mr-4 hover:bg-yellow-300 hover:text-black'>Get Started</button>
-            </Link>
-            <Link href="https://chrome.google.com/webstore/detail/leadzilla/mlknnmdepgmefemphhdombdflfgceejg">
-              <button className='text-white text-sm font-bold border-2 hover:bg-yellow-300 hover:text-black  py-4 px-8 rounded-full'>Add Chrome Extansion</button>
+            <div className="flex flex-row items-center">
+              <Link href="https://app.leadzilla.ai/login">
+                <button className="text-white text-sm font-bold border-2 py-4 px-8 rounded-full mr-4 hover:bg-yellow-300 hover:text-black">
+                  Get Started
+                </button>
               </Link>
-           </div>
+              <Link href="https://chrome.google.com/webstore/detail/leadzilla/mlknnmdepgmefemphhdombdflfgceejg">
+                <button className="text-white text-sm font-bold border-2 hover:bg-yellow-300 hover:text-black  py-4 px-8 rounded-full">
+                  Add Chrome Extansion
+                </button>
+              </Link>
+            </div>
           </div>
 
-          {/* <div className='flex flex-row justify-between mt-16'> */}
-            <div className='flex justify-between mt-16'>
-              <div className='font-gilroybold text-white text-lg mb-5'><Link href="/features">Features</Link></div>
-              {/* <p className='text-gray-500 my-2 cursor-pointer'><Link href="/features">Features</Link></p>
+          {/* Disabling link while FAQ page is under development
+           <div className='flex flex-row justify-between mt-16'> */}
+
+          <div className="flex justify-between mt-16">
+            <div className="font-gilroybold text-white text-lg mb-5">
+              <Link href="/features">Features</Link>
+            </div>
+            {/* <p className='text-gray-500 my-2 cursor-pointer'><Link href="/features">Features</Link></p>
               <p className='text-gray-500 my-2 cursor-pointer'> <Link href="https://blog.leadzilla.ai"> Blogs</Link></p>
               <p className='text-gray-500 my-2 cursor-pointer'><Link href="/faq">FAQ</Link></p>
               <p className='text-gray-500 my-2 cursor-pointer'><Link href="/contactus">Contacts</Link></p> */}
-              <div className='font-gilroybold text-white text-lg mb-5'><Link href="https://blog.leadzilla.ai">
-                    Blogs
-                    </Link></div>
-              <div className='font-gilroybold text-white text-lg mb-5'><Link href="/faq">FAQ</Link></div>
-              <div className='font-gilroybold text-white text-lg mb-5'><Link href="/contactus">Contact Us</Link></div>
+            <div className="font-gilroybold text-white text-lg mb-5">
+              <Link href="https://blog.leadzilla.ai">Blogs</Link>
             </div>
-            {/* <div className='flex flex-col'>
+            {/* <div className='font-gilroybold text-white text-lg mb-5'><Link href="/faq">FAQ</Link></div> */}
+            <div className="font-gilroybold text-white text-lg mb-5">
+              <Link href="/contactus">Contact Us</Link>
+            </div>
+          </div>
+          {/* <div className='flex flex-col'>
             </div>
             <div className='flex flex-col'>
             </div>
             <div className='flex flex-col'>
             </div> */}
-            <div className='flex flex-col'>
-              {/* <p className='font-gilroybold text-white text-lg mb-5'>Legal</p> */}
-            </div>
+          <div className="flex flex-col">
+            {/* <p className='font-gilroybold text-white text-lg mb-5'>Legal</p> */}
+          </div>
           {/* </div> */}
-          <div className='flex justify-between items-center mt-20'>
-            <p className='text-gray-400'>©Copyright 2022 Leadzilla</p>
-            <div className='flex flex-row'>
-              <div className='flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2'>
-                <Image src="/assets/facebook.png" alt="facebook" height="25" width="18" />
+          <div className="flex justify-between items-center mt-20">
+            <p className="text-gray-400">©Copyright 2022 Leadzilla</p>
+            <div className="flex flex-row">
+              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2">
+                <Image
+                  src="/assets/facebook.png"
+                  alt="facebook"
+                  height="25"
+                  width="18"
+                />
               </div>
-              <div className='flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2'>
-                <Image src="/assets/linkedIn.png" alt="facebook" height="25" width="25" />
+              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2">
+                <Image
+                  src="/assets/linkedIn.png"
+                  alt="facebook"
+                  height="25"
+                  width="25"
+                />
               </div>
-              <div className='flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2'>
-                <Image src="/assets/youtube.png" alt="facebook" height="18" width="25" />
+              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2">
+                <Image
+                  src="/assets/youtube.png"
+                  alt="facebook"
+                  height="18"
+                  width="25"
+                />
               </div>
-              <div className='flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2'>
-                <Image src="/assets/instagram.png" alt="facebook" height="25" width="25" />
+              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-gray-500 cursor-pointer mx-2">
+                <Image
+                  src="/assets/instagram.png"
+                  alt="facebook"
+                  height="25"
+                  width="25"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Footer;
--- components/homepage/cutomerReviews.js
+++ components/homepage/cutomerReviews.js
@@ -2,42 +2,43 @@ import Carousel from "react-multi-carousel";
 import React from "react";
 import "react-multi-carousel/lib/styles.css";
 import CarouselContent from "./carouselContent";
-import Image from 'next/image'
+import Image from "next/image";
 
 function CutomerReviews() {
-
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 1,
-      slidesToSlide: 1 // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 1,
-      slidesToSlide: 1 // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
+      slidesToSlide: 1, // optional, default to 1.
+    },
   };
 
-  const customerReviews=[
+  const customerReviews = [
     {
-      description: "After checking all the alternatives, Leadzilla was the only solution to provide so much more than that. On top of that, it was also the only solution that didn’t involve so much pain during integration, so quick and easy, my team loves it.",
-      img: "/assets/personleadzilla.svg",
-      name: "James Smith",
-      designation: "Sales Manager @Company name",
+      description:
+        "After checking all the alternatives, Leadzilla was the only solution to provide so much more than that. On top of that, it was also the only solution that didn’t involve so much pain during integration, so quick and easy, my team loves it.",
+      img: "/assets/customers/connor-hevingham-alphaletz.jpeg",
+      name: "Connor Hevingham",
+      designation: "Head of Marketing @ Alphaletz",
     },
     {
-      description: "We checked out a number of data products and we just loved the simplicity of Leadzilla. The data is second to none and the AI module works like magic.",
-      img: "/assets/secondreview.jpeg",
+      description:
+        "We checked out a number of data products and we just loved the simplicity of Leadzilla. The data is second to none and the AI module works like magic.",
+      img: "/assets/customers/patricia-christensen-goperspecta.jpeg",
       name: "Patricia Christensen ",
       designation: "Sales Manager @ Goperspecta",
-    }
-  ]
+    },
+  ];
   const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
     const {
       carouselState: { currentSlide },
@@ -46,16 +47,28 @@ function CutomerReviews() {
       <div className="carousel-button-group">
         <div className="flex justify-end">
           <button
-            className='mx-2 h-12 w-12 bg-yellow-500 flex justify-center items-center'
+            className="mx-2 h-12 w-12 bg-yellow-500 flex justify-center items-center"
             onClick={() => previous()}
           >
-            <Image src="/assets/arrowBlackLeft.png" alt="next" height="16px" width="20px" className="py-1 px-2" />
+            <Image
+              src="/assets/arrowBlackLeft.png"
+              alt="next"
+              height="16px"
+              width="20px"
+              className="py-1 px-2"
+            />
           </button>
           <button
-            className='mx-2 h-12 w-12 bg-yellow-500 flex justify-center items-center'
+            className="mx-2 h-12 w-12 bg-yellow-500 flex justify-center items-center"
             onClick={() => next()}
           >
-            <Image src="/assets/arrowBlackRight.png" alt="previous" height="16px" width="20px" className="py-1 px-2" />
+            <Image
+              src="/assets/arrowBlackRight.png"
+              alt="previous"
+              height="16px"
+              width="20px"
+              className="py-1 px-2"
+            />
           </button>
         </div>
       </div>
@@ -67,7 +80,7 @@ function CutomerReviews() {
       onMove,
       index,
       active,
-      carouselState: { currentSlide, deviceType }
+      carouselState: { currentSlide, deviceType },
     } = rest;
     const carouselItems = [1, 2];
     // onMove means if dragging or swiping in progress.
@@ -76,9 +89,7 @@ function CutomerReviews() {
       <button
         className={`${active ? "active" : "inactive"} h-2 w-8 bg-gray-600 mx-1`}
         onClick={() => onClick()}
-      >
-
-      </button>
+      ></button>
     );
   };
 
@@ -86,7 +97,9 @@ function CutomerReviews() {
     <>
       <div className="flex flex-col justify-center items-center py-8">
         <p className="text-5xl font-gilroybold">See what our customers</p>
-        <p className="font-light text-5xl font-gilroylight pb-2">have to say for us</p>
+        <p className="font-light text-5xl font-gilroylight pb-2">
+          have to say for us
+        </p>
         <div className="w-10/12">
           <Carousel
             swipeable={true}
@@ -103,16 +116,14 @@ function CutomerReviews() {
             customButtonGroup={<ButtonGroup />}
             customDot={<CustomDot />}
           >
-            {customerReviews.map((val)=>{
-              return(
-                <CarouselContent val={val}/>
-              )
+            {customerReviews.map((val, index) => {
+              return <CarouselContent val={val} key={index} />;
             })}
-          </Carousel>;
+          </Carousel>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default CutomerReviews;
