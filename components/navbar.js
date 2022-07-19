beb23243b1aca3e186e5921200e6800ab96d8af2
reactexcel <ashutoh.excel2011@gmail.com>
2022-07-19 12:28:18 +0530

    faq responsiveness done

--- components/navbar.js
+++ components/navbar.js
@@ -3,11 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [toggle, setToggle]= useState(false)
-
-
-
-  console.log(toggle, "KKKKKKKKKKKKKKKKKKKKKKKKK")
+  const [toggle, setToggle] = useState(false)
   return (
     <>
       <nav className="sm:bg-blue-300 sm:hidden">
@@ -64,8 +60,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-
-
       <nav className="bg-blue-300 border-gray-200 dark:border-gray-600 dark:bg-gray-900 hidden sm:block">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
           <Link href="/">
@@ -76,11 +70,11 @@ const Navbar = () => {
               height="38px"
             />
           </Link>
-          <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false" onClick={(e)=>setToggle(!toggle)}>
+          <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false" onClick={(e) => setToggle(!toggle)}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           </button>
-          <div id="mega-menu-full" className={` ${toggle? "justify-between items-center w-full md:flex md:w-auto md:order-1": "hidden justify-between items-center w-full md:flex md:w-auto md:order-1"}`}>
+          <div id="mega-menu-full" className={` ${toggle ? "justify-between items-center w-full md:flex md:w-auto md:order-1" : "hidden justify-between items-center w-full md:flex md:w-auto md:order-1"}`}>
             <ul className="flex flex-col mt-4 text-sm font-medium md:flex-row md:space-x-8 md:mt-0">
               <li>
                 <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100" aria-current="page">Solutions</a>
