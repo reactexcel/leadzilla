884eb5e313e5f6a8695e891774418b2340c1fa67
reactexcel <ashutoh.excel2011@gmail.com>
2022-08-23 15:02:05 +0530

    fixed the header for mobile and added sign in, book CTOs

--- components/header.js
+++ components/header.js
@@ -1,64 +1,84 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 const Header = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="hidden sm:block md:block border-gray-200 px-2 md:px-4 py-2.5 rounded dark:bg-gray-900">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <Link href="/">
+    <nav className={`hidden sm:block md:block border-gray-200 px-2 md:px-4 py-2.5 rounded dark:bg-gray-900 shadow-md ${toggle && 'bg-gray-50 z-10'}`}>
+      <div className='container flex flex-wrap justify-between items-center mx-auto'>
+        <Link href='/'>
           <Image
-            src="/assets/logoleadzilla.svg"
-            alt=""
-            width="120px"
-            height="38px"
+            src='/assets/logoleadzilla.svg'
+            alt=''
+            width='120px'
+            height='38px'
           />
         </Link>
         <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg"
-          aria-controls="navbar-default"
-          aria-expanded="false"
+          data-collapse-toggle='navbar-default'
+          type='button'
+          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg'
+          aria-controls='navbar-default'
+          aria-expanded='false'
           onClick={(e) => setToggle(!toggle)}
         >
-         <Image src="/assets/hamburger.svg" width="30" height="30"/>
-        
+          <Image src='/assets/hamburger.svg' width='30' height='30' />
         </button>
-        <div className={`${toggle ? "" : "hidden"} w-full`} id="navbar-default">
-          <ul className="flex flex-col ml-3 mt-4 md:mt-2 md:text-md">
-            <li className="sm:my-2 md:my-2 font-semibold">
+        <div className={`${toggle ? '' : 'hidden'} w-full`} id='navbar-default'>
+          <ul className='flex flex-col ml-3 mt-4 md:mt-2 md:text-md'>
+            <li className='sm:my-2 md:my-2 font-semibold'>
               <Link
-                href="/pricing"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 "
+                href='/pricing'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 '
               >
                 Pricing
               </Link>
             </li>
-            <li className="sm:my-2 md:my-2 font-semibold">
+            <li className='sm:my-2 md:my-2 font-semibold'>
               <Link
-                href="/features"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 "
+                href='/features'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 '
               >
                 Features
               </Link>
             </li>
-            <li className="sm:my-2 md:my-2 font-semibold">
+            <li className='sm:my-2 md:my-2 font-semibold'>
               <Link
                 href='https://blog.leadzilla.ai'
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 "
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 '
               >
                 Blogs
               </Link>
             </li>
-            <li className="sm:my-2 md:my-2 font-semibold">
+            <li className='sm:my-2 md:my-2 font-semibold'>
               <Link
-                href="/about"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 "
+                href='/about'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100'
               >
                 About
               </Link>
+            </li>
+            <hr/>
+            <li className='sm:my-2 md:my-2 font-semibold'>
+              <button className="border-2 px-2 mr-3 py-2 text-base font-medium w-full hover:bg-black hover:text-white">
+              <Link
+                href='https://app.leadzilla.ai/login'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100'
+              >
+                Sign up
+              </Link>
+              </button>
+            </li>
+            <li className='sm:my-2 md:my-2 font-semibold'>
+            <button className="border-2 px-2 mr-3 py-2 text-base font-medium w-full hover:bg-black hover:text-white">
+              <Link
+                href='https://calendly.com/sam_gupta/15min'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100'
+              >
+                Book a demo
+              </Link>
+              </button>
             </li>
           </ul>
         </div>
