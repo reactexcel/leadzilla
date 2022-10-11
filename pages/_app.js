b680aaf54494eeb860517a9b11ea750ee60b11ff
Vikas Singh <vikas3091999@gmail.com>
2022-10-11 15:03:11 +0530

    added chat widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import Meta from "./meta";
-import Head from 'next/head';
+import Head from "next/head";
+import CripChatWidget from "../components/crispChatWidget";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-     <Head>
-      <link rel="shortcut icon" href="/assets/favicon.svg" />
-    </Head>
+      <Head>
+        <link rel="shortcut icon" href="/assets/favicon.svg" />
+      </Head>
       <Meta />
       <Component {...pageProps} />
+      <CripChatWidget />
     </>
-  )
+  );
 }
 
 export default MyApp;
